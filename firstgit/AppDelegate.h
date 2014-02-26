84f7d971f3ba24bfe92a85749e3d093cf095c88a
dhwanilshah <dcs426@nyu.edu>
2014-02-25 19:19:19 -0500

    Initial Commit

---
+++ firstgit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  firstgit
+//
+//  Created by Dhwanil Shah on 2/25/14.
+//  Copyright (c) 2014 Dhwanil Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
